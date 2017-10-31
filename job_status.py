0401a11eb597b85c3c3a5ecded21579b96e33047
Dustin <dustin.spears@live.com>
2017-10-31 11:55:29 -0400

    Update job_status.py

--- job_status.py
+++ job_status.py
@@ -1,7 +1,7 @@
 from errbot import BotPlugin, botcmd
 
 class AutoSys(BotPlugin):
-    """Example AutoSys plugin for Errbot."""
+    """Example AutoSys plugin for Errbot"""
 
     @botcmd
     def job_status(self, msg, args):
